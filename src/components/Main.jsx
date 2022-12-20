954e09db9a3d8e893504cc0e0130e0344fee5398
pijeismart <reactdeveloper1990@gmail.com>
2022-12-20 20:02:48 +0100

    added logic for filter

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,12 +2,27 @@ import React, { useContext, useState } from "react";
 import SearcIconWhite from "../assets/SearchIconWhite";
 import SearchIcon from "../assets/SearchIcon";
 import ModeContext from "../context/ModeContext";
+import CountriesContext from "../context/CountriesContext";
 import Countries from "./Countries";
 import Search from "../constants/Search";
 import Filter from "../constants/Filter";
+import { API_URL } from "../util/api";
 
 const Main = () => {
   const { mode } = useContext(ModeContext);
+  const { setCountries } = useContext(CountriesContext);
+
+
+  const getCountryByRegion = async (regionName) => {
+    try {
+      const res = await fetch(`${API_URL}/region/${regionName}`);
+      if (!res.ok) throw new Error("Failed..........");
+      const data = await res.json();
+      setCountries(data);
+    } catch (error) {
+      setError(false);
+    }
+  };
 
   return (
     <main
@@ -20,7 +35,7 @@ const Main = () => {
           {mode ? <SearchIcon /> : <SearcIconWhite />}
           <Search />
         </div>
-        <Filter />
+        <Filter onSelect={getCountryByRegion} />
       </div>
 
       <div className="bottom pt-[40px] flex flex-wrap justify-between">
--- src/constants/Filter.jsx
+++ src/constants/Filter.jsx
@@ -1,25 +1,32 @@
-import React, {useContext} from "react";
+import React, { useContext, useState } from "react";
 import ModeContext from "../context/ModeContext";
 
-const Filter = () => {
+const Filter = ({onSelect}) => {
   const { mode } = useContext(ModeContext);
-  
+
+  const selectType = (event) => {
+    const regionName = event.target.value;
+    onSelect(regionName);
+  };
 
   return (
-    <select
-      className={`${
-        mode ? "bg-white" : "bg-dark-darkBlue"
-      } w-[250px] px-[30px] py-[15px] rounded-md shadow-lg transition-all duration-500 border-0 cursor-pointer focus:outline-0`}
-      name="filter"
-      id=""
-    >
-      <option>Filter by Region</option>
-      <option value="Africa">Africa</option>
-      <option value="America">America</option>
-      <option value="Asia">Asia</option>
-      <option value="Europe">Europe</option>
-      <option value="Oceania">Oceania</option>
-    </select>
+    <>
+      <select
+        onChange={selectType}
+        className={`${
+          mode ? "bg-white" : "bg-dark-darkBlue"
+        } w-[250px] px-[30px] py-[15px] outline-0 rounded-md shadow-lg transition-all duration-500 border-0 cursor-pointer`}
+        name="filter"
+        id=""
+      >
+        <option>Filter by Region</option>
+        <option value="Africa">Africa</option>
+        <option value="America">America</option>
+        <option value="Asia">Asia</option>
+        <option value="Europe">Europe</option>
+        <option value="Oceania">Oceania</option>
+      </select>
+    </>
   );
 };
 
